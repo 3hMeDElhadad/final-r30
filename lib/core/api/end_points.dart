c1d1acb6876bfe7e12510da885f016b3d440b08f
moheb2002 <100113379+moheb2002@users.noreply.github.com>
2024-09-06 19:44:59 +0300

    api

---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,10 @@
+class EndPoints {
+  static const String baseurl = 'https://fake-store-api.mock.beeceptor.com';
+  static const String products = '/api/products';
+  static const String carts = '/api/carts';
+}
+
+class ApiKeys {
+  static String status = 'status';
+  static String message = 'ErrorMessage';
+}
